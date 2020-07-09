2838ba85343e126a8c3fe8d3c55bd648c858aa6b
Andreas Ulrich <andreas@ulrich.com.de>
2020-07-09 10:09:43 +0200

    root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "model1s#index"
   resources :model2s
   resources :model1s
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
